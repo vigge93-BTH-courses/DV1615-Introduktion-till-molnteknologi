8348e6573be2470d7ea4fb18d452567efd911c75
Victor <victarv@gmail.com>
2021-02-28 22:58:37 +0100

    Small fixes to final project

--- Projekt/Webapp/app.py
+++ Projekt/Webapp/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, flash, jsonify
+from flask import Flask, render_template, request, flash, jsonify, redirect, url_for
 import requests
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Union, Optional
 import json
 
 app = Flask(__name__)
@@ -11,8 +11,13 @@ cognitive_services_endpoint = 'https://dv1615-apimanagement-lab.azure-api.net'
 cognitive_services_access_token = '6af7226881af4bf4a83ccd810023a5a0'
 
 
+@app.route('/')
+def index() -> Any:
+    return redirect(url_for('image_search'))
+
+
 @app.route('/image_search', methods=('GET', 'POST'))
-def image_search():
+def image_search() -> str:
 
     if request.method == 'POST':
         image_url = request.form['image-url']
@@ -35,14 +40,14 @@ def image_search():
     return render_template('index.html')
 
 
-def get_data() -> Dict[str, int]:
+def get_data() -> List[Dict[str, Any]]:
     response = requests.get(stock_api_endpoint)
-    response_data = response.json()['data']
+    response_data: List[Dict[str, Any]] = response.json()['data']
     return response_data
 
 
-def process_data(raw_data) -> Dict[str, int]:
-    data: Dict[str, int] = {}
+def process_data(raw_data: List[Dict[str, Any]]) -> Dict[str, Dict[str, Any]]:
+    data: Dict[str, Dict[str, Union[str, int]]] = {}
     for d in raw_data:
         if not isinstance(d['stock'], int):
             d['stock'] = 0
@@ -55,7 +60,7 @@ def process_data(raw_data) -> Dict[str, int]:
     return data
 
 
-def process_image(img_url: str) -> str:
+def process_image(img_url: str) -> Optional[List[str]]:
     url = f'{cognitive_services_endpoint}/vision/v2.0/analyze?visualFeatures=Tags'
     body = {'url': img_url}
     headers = {'Ocp-Apim-Subscription-Key': cognitive_services_access_token}
@@ -77,7 +82,7 @@ def process_image(img_url: str) -> str:
     return [d['name'] for d in data if d['confidence'] > 0.99]
 
 
-def translate_text(texts: List[str]) -> str:
+def translate_text(texts: List[str]) -> Optional[List[str]]:
     url = f'{cognitive_services_endpoint}/translate?api-version=3.0&from=en&to=sv'
     body = [{'text': t} for t in texts]
     headers = {'Ocp-Apim-Subscription-Key': cognitive_services_access_token}
@@ -98,7 +103,7 @@ def translate_text(texts: List[str]) -> str:
     return result
 
 
-def search_data(data: Dict[str, int], query: str) -> Dict[str, int]:
+def search_data(data: Dict[str, Dict[str, Any]], query: str) -> List[Dict[str, Union[int, str]]]:
     query = query.lower()
     response_data = [{'name': name, 'stock': value['stock'],
                       'description': value['description']}
